6ea31d361d5fdeafbd37d2ffe05921d80cefcf17
Daniel <danielc9912@gmail.com>
2023-11-22 18:21:03 -0500

    analytics fix fr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,15 +46,15 @@ export default function App({ Component, pageProps }: AppProps) {
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-PH94PQX5DQ"
       ></Script>
-      <Script>
+      <Script id="google-analytics">
         {`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-PH94PQX5DQ');
-`}
+  gtag('config', 'G-PH94PQX5DQ');`}
       </Script>
+
       <Component {...pageProps} />
     </>
   );
